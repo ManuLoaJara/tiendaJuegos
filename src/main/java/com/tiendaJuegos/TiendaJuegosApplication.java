f1ad0bb8943be8ad4bc9fd5e43cda90699abbc1d
manuloajara04 <manuela@loaizas.com>
2020-06-21 16:10:17 -0500

    First Commit

---
+++ src/main/java/com/tiendaJuegos/TiendaJuegosApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaJuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJuegosApplication.class, args);
+	}
+
+}
